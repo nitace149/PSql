07eb4fb863079d49ff75b2c15b2a398b837f8ad6
Nitesh <niteshtyagi149@gmail.com>
2025-09-02 23:47:19 +0530

    connect postgresql with node js

---
+++ postgresql/readme.md
@@ -0,0 +1,3 @@
+https://medium.com/@mavericks-db/how-to-connect-postgresql-with-node-js-bd2cbe87aa9e
+
+https://stackoverflow.com/questions/9205496/how-to-make-connection-to-postgres-via-node-js
---
+++ postgresql/src/index.ts
@@ -0,0 +1,18 @@
+import { Client } from 'pg';
+
+const pgClient = new Client ({
+user: "neondb_owner", password: "wrWG5KI1ziYB",
+port: 5432,
+host: "ep-lucky-snow-a50il0b5.us-east-2-aws. neon.tech",
+database: "neondb"
+})
+// const pgClient = new Client ({"Object | String"}) 
+// HERE WE CAN PASS OBJECT AS WELL AS CONNECTION STRING OF CLOUD AND http://localhost: URL
+
+
+async function main(){
+await pgClient.connect();  // ===========> this will call the connect function to connect the database with above crediential
+const response = await pgClient.query("SELECT * FROM users");
+console.log(response);
+
+}
